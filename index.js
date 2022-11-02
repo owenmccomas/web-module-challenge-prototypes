f9cef25ffdd474acb0da7899127b238511e47cbd
Owen McComas <owen@mccomas.me>
2022-11-01 22:00:20 -0600

    First task and partial second finished

--- index.js
+++ index.js
@@ -15,10 +15,22 @@
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name, age) {
+  this.name = name
+  this.age = age
+  this.stomach = [];
 }
-
+Person.prototype.eat = function(item) 
+{ if (this.stomach.length <= 10) { 
+  return this.stomach.push(item);
+ } else {return "Stomach is full"}
+}
+Person.prototype.poop = function(){
+  this.stomach = [];
+}
+Person.prototype.toString = function(){
+  return `${this.name}, ${this.age}`
+ }
 
 /*
   TASK 2
@@ -36,8 +48,14 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model, milesPerGallon) {
+  this.model = model
+  this.milesPerGallon = milesPerGallon
+  this.tank = 0
+  this.odometer = 0
+}
+Car.prototype.fill = function(gallons){
+  return this.odometer + gallons
 }
 
 
